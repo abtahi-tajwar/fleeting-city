f731c9a6f4e2f3ce7a2b3aaf4b3e0e8a1a6f77b4
abtahi-tajwar <abtahitajwar@gmail.com>
2025-07-14 22:31:51 -0400

    Move character on point

--- Units/Player/CharacterMovement.cs
+++ Units/Player/CharacterMovement.cs
@@ -6,6 +6,8 @@ public partial class CharacterMovement : CharacterBody2D
     [Export]
     public TileMapLayer Ground { get; set; }
     private AStarGrid2D _grid;
+    private Godot.Collections.Array<Vector2I> _idPath;
+    private Vector2I _targetPosition;
 
     public override void _Ready()
     {
@@ -13,20 +15,46 @@ public partial class CharacterMovement : CharacterBody2D
         Rect2I groundCoords = Ground.GetUsedRect();
         _grid.Region = groundCoords;
         _grid.CellSize = new Vector2I(Ground.RenderingQuadrantSize, Ground.RenderingQuadrantSize);
+        _grid.DiagonalMode = AStarGrid2D.DiagonalModeEnum.Always;
+        _grid.Update();
+    }
+    public override void _PhysicsProcess(double delta)
+    {
+        if (_idPath != null)
+        {
+            if (_idPath.Count == 0)
+            {
+                _idPath = null; // Clear the path when done
+                return;
+            }
+
+            Vector2I nextPosition = _idPath[0];
+            Vector2 targetPosition = Ground.MapToLocal(nextPosition);
+            GlobalPosition = GlobalPosition.MoveToward(targetPosition, 100f * (float)delta);
+            if (GlobalPosition.DistanceTo(targetPosition) < 1f)
+            {
+                _idPath.RemoveAt(0);
+                if (_idPath.Count == 0)
+                    _idPath = null;
+            }
+            GD.Print("Current ID Path: ", targetPosition, _idPath);
+
+        }
     }
 
     public override void _Input(InputEvent @event)
     {
         if (@event is InputEventMouseButton mouseButtonEvent && mouseButtonEvent.IsPressed())
         {
-            Vector2 mousePosition = mouseButtonEvent.Position;
-            Vector2I mouseGridPos= Ground.LocalToMap(mousePosition);
-            Vector2I globalMousePos = Ground.LocalToMap(GetGlobalMousePosition());
-            Vector2 playerPos = Ground.LocalToMap(GlobalPosition);
-            GD.Print("Player Position: ", playerPos );
-            GD.Print("Global Mouse Position: ", globalMousePos);
-            GD.Print("Mouse Position: ", mouseGridPos.X/16, ", ", mouseGridPos.Y/16);
-            
+            Vector2I targetPos = Ground.LocalToMap(GetGlobalMousePosition());
+            Vector2I playerPos = Ground.LocalToMap(GlobalPosition);
+            GD.Print("Player Position: ", playerPos);
+            GD.Print("Global Mouse Position: ", targetPos);
+
+            _targetPosition = targetPos;
+            _idPath = _grid.GetIdPath(playerPos, targetPos);
+            GD.Print("Path found: ", _idPath);
+            _idPath.RemoveAt(0); // Remove the first element which is the current position
         }
 
     }
