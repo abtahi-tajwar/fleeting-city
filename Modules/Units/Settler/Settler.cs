911a5d6a5eef721ebf51ecf191b823d6ba9eb696
abtahi-tajwar <abtahitajwar@gmail.com>
2025-07-23 21:05:19 -0400

    Settler movement implemented

--- Modules/Units/Settler/Settler.cs
+++ Modules/Units/Settler/Settler.cs
@@ -4,50 +4,50 @@ using System;
 
 public partial class Settler : CharacterBody2D
 {
-    public string UniqueId { get; set; }
+	public string UniqueId { get; set; }
 
-    // Services
-    private CharacterMovementService _movementService;
-    private CharacterAnimationService _animationService;
-    private UnitSelectionManager _unitSelectionManager;
+	// Services
+	private CharacterMovementService _movementService;
+	private CharacterAnimationService _animationService;
+	private UnitSelectionManager _unitSelectionManager;
 
-    public override void _Ready()
-    {
-        UniqueId = Guid.NewGuid().ToString(); // Assign a unique ID to the settler
-        GD.Print($"Settler created with UniqueId: {UniqueId}");
-        _movementService = new CharacterMovementService(this);
-        UnitSelectionManager.SetOutline(this, false);
+	public override void _Ready()
+	{
+		UniqueId = Guid.NewGuid().ToString(); // Assign a unique ID to the settler
+		GD.Print($"Settler created with UniqueId: {UniqueId}");
+		_movementService = new CharacterMovementService(this);
+		UnitSelectionManager.SetOutline(this, false);
 
-        _animationService = new CharacterAnimationService(this);
-        _unitSelectionManager = new UnitSelectionManager(this);
-    }
-    public override void _PhysicsProcess(double delta)
-    {
-        MOVEMENT_DIRECTION_ENUM moveDirection = _movementService.Update(delta);
-        _animationService.PlayWalkAnimation(moveDirection);
-    }
-    public override void _Process(double delta)
-    {
-        // Additional processing logic can be added here if needed
-        if (UnitSelectionManager.SelectedSettlerId == UniqueId)
-        {
-            UnitSelectionManager.SetOutline(this, true);
-        }
-        else
-        {
-            UnitSelectionManager.SetOutline(this, false);
-        }
-    }
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        if (
-            @event is InputEventMouseButton mouseButtonEvent
-            && mouseButtonEvent.IsPressed()
-            // && UnitSelectionManager.SelectedSettlerId == UniqueId
-            // && ActionManager.CurrentAction == ACTION_ENUM.MOVE
-        )
-        {
-            if (ActionManager.CurrentAction == ACTION_ENUM.MOVE)
+		_animationService = new CharacterAnimationService(this);
+		_unitSelectionManager = new UnitSelectionManager(this);
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		MOVEMENT_DIRECTION_ENUM moveDirection = _movementService.Update(delta);
+		_animationService.PlayWalkAnimation(moveDirection);
+	}
+	public override void _Process(double delta)
+	{
+		// Additional processing logic can be added here if needed
+		if (UnitSelectionManager.SelectedSettlerId == UniqueId)
+		{
+			UnitSelectionManager.SetOutline(this, true);
+		}
+		else
+		{
+			UnitSelectionManager.SetOutline(this, false);
+		}
+	}
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (
+			@event is InputEventMouseButton mouseButtonEvent
+			&& mouseButtonEvent.IsPressed()
+			// && UnitSelectionManager.SelectedSettlerId == UniqueId
+			// && ActionManager.CurrentAction == ACTION_ENUM.MOVE
+		)
+		{
+			if (ActionManager.CurrentAction == ACTION_ENUM.MOVE)
 			{
 				if (UnitSelectionManager.SelectedSettlerId == UniqueId)
 				{
@@ -58,15 +58,6 @@ public partial class Settler : CharacterBody2D
 			{
 				_unitSelectionManager.SelectPointedUnit(GetGlobalMousePosition());
 			}
-        }
-    }
-    // public override void _InputEvent(Viewport viewport, InputEvent @event, int shapeIdx)
-    // {
-    //     if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed && mouseEvent.ButtonIndex == MouseButton.Left)
-    //     {
-    //         UnitSelectionManager.SelectUnit(this);
-    //     }
-    // }
-
-    
+		}
+	}
 }
