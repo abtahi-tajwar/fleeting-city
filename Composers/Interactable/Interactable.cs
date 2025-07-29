bf6c69a9d5fdc31b49f1e6effa793246daf7852d
abtahi-tajwar <abtahitajwar@gmail.com>
2025-07-29 19:05:08 -0400

    Interactable composer, animalfarm subsystem implemented

---
+++ Composers/Interactable/Interactable.cs
@@ -0,0 +1,49 @@
+using Godot;
+using System;
+
+public partial class Interactable : Node
+{
+	private Area2D _area;
+
+	// Signals
+	[Signal]
+	public delegate void InteractionStartEventHandler(CharacterBody2D character);
+	[Signal]
+	public delegate void InteractionEndEventHandler(CharacterBody2D character);
+
+	public override void _Ready()
+	{
+		_area = GetNode<Area2D>("InteractBoundary");
+		if (_area == null)
+		{
+			GD.PrintErr("Area2D node not found in Interactable.");
+		}
+		else
+		{
+			_area.BodyEntered += OnBodyEntered;
+			_area.BodyExited += OnBodyExited;
+		}
+	}
+
+	private void OnBodyEntered(Node body)
+	{
+		if (body is Player player)
+		{
+			EmitSignal(SignalName.InteractionStart, player);
+		}
+		else if (body is Settler settler)
+		{
+			EmitSignal(SignalName.InteractionStart, settler);
+		}
+	}
+	private void OnBodyExited(Node body)
+	{
+		if (body is Player player)
+		{
+			EmitSignal(SignalName.InteractionEnd, player);
+		} else if (body is Settler settler)
+		{
+			EmitSignal(SignalName.InteractionEnd, settler);
+		}
+	}   
+}
