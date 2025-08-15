34143923bc0e8e67766fa34e082856ead7568099
abtahi-tajwar <abtahitajwar@gmail.com>
2025-08-14 22:27:00 -0400

    Project aspect settings updated

--- Debug/InputTracer/InputTracer.cs
+++ Debug/InputTracer/InputTracer.cs
@@ -3,20 +3,25 @@ using System;
 
 public partial class InputTracer : Node
 {
-    public override void _Ready() => Hook(GetTree().Root);
+	public override void _Ready() => Hook(GetTree().Root);
 
-    private void Hook(Node n)
-    {
-        foreach (var child in n.GetChildren())
-        {
-            if (child is Control c)
-                c.GuiInput += (InputEvent ev) => GD.Print($"[GUI] {c.GetPath()} got {ev}");
+	private void Hook(Node n)
+	{
+		foreach (var child in n.GetChildren())
+		{
+			if (child is Control c)
+				c.GuiInput += (InputEvent ev) =>
+				{
+					// GD.Print($"[GUI] {c.GetPath()} got {ev}");
+					ProcessDebug.Print($"[GUI] {c.GetPath()} got {ev}");
+				};
 
-            if (child is CollisionObject2D co)
-                co.InputEvent += (Node vp, InputEvent ev, long idx) =>
-                    GD.Print($"[PHYS] {co.GetPath()} got {ev}");
+			if (child is CollisionObject2D co)
+				co.InputEvent += (Node vp, InputEvent ev, long idx) =>
+					// GD.Print($"[PHYS] {co.GetPath()} got {ev}");
+					ProcessDebug.Print($"[PHYS] {co.GetPath()} got {ev}");
 
-            Hook(child);
-        }
-    }
+			Hook(child);
+		}
+	}
 }
